d7594e83fae183d25ebb6e529befc33f2fe11a88
LazyHat <sevapetrov2016p@gmail.com>
2023-10-19 23:36:25 +0300

    0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 11
-        versionName = "0.1"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
